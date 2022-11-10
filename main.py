7c2fec2ba43682e64c614e8cecc40860456fd805
zebdelrosario <delrosariozeb@gmail.com>
2022-11-10 23:21:44 +1000

    Add resource time summary

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ import os.path
 import pandas as pd
 from openpyxl import load_workbook
 
-TICKETS_RAW_DATA = 'test_files/Ticket Search Results (2).csv'
+RAW_DATA = input('Input file name:\n>')
 DESTINATION_SHEET = 'test_files/'
 
 
 def main():
     """Main code block."""
-    ticket_df = pd.read_csv(TICKETS_RAW_DATA)
-    file_name = 'test_01.xlsx'
+    tickets_raw_data = f'test_files/{RAW_DATA}.csv'
+    ticket_df = pd.read_csv(tickets_raw_data)
+    ticket_resources = get_unique_entries(ticket_df, 'Resources')
     issue_summary = get_count(ticket_df, 'Issue Type')
     resource_summary = get_count(ticket_df, 'Resources')
     total_time_worked = ticket_df['Total Hours Worked'].sum()
@@ -19,12 +20,38 @@ def main():
     all_overdue_tickets = get_all_overdue_tickets(ticket_df, 'Due', 'Resolved Time')
     #   TURN LIST INTO DATAFRAME
     overdue_df = get_overdue_summary(ticket_df, all_overdue_tickets)
+#     Get resource time summary
+    resource_time_summary = get_unique_summary(ticket_df, 'Resources', 'Total Hours Worked')
+    # print(resource_summary)
+    complete_resource_summary = pd.concat([resource_time_summary, resource_summary.rename('Tickets Assigned')], axis=1)
+    print('Preparing Data...')
 #   Write to EXCEL
-    with pd.ExcelWriter(file_name) as writer:
+    out_file_name = input('Output file name:\n>') + '.xlsx'
+    with pd.ExcelWriter(out_file_name) as writer:
         ticket_df.to_excel(writer, sheet_name='raw_data')
+        print('Preparing File')
         issue_summary.to_excel(writer, sheet_name='summary', startcol=0)
-        resource_summary.to_excel(writer, sheet_name='summary', startcol=4)
+        print('Writing Issue Types...')
+        complete_resource_summary.to_excel(writer, sheet_name='summary', startcol=4)
+        print('Writing Resource summary...')
         overdue_df.to_excel(writer, sheet_name='summary', startcol=8)
+        print('Writing Overdue tickets...')
+
+
+def get_unique_summary(dataframe, column_one, column_two):
+    """Summarize column based on unique entries."""
+    summary = dataframe.groupby(column_one)[column_two].sum()
+    return summary
+
+
+def get_unique_entries(dataframe, column):
+    """Get a list of resources."""
+    column_to_dissect = dataframe[column]
+    unique_entries = []
+    for i in column_to_dissect:
+        if i not in unique_entries:
+            unique_entries.append(i)
+    return unique_entries
 
 
 def get_overdue_summary(dataframe, overdue_tickets):
@@ -37,7 +64,7 @@ def get_overdue_summary(dataframe, overdue_tickets):
 
 def get_count(dataframe, column):
     """Get total count of entries in column."""
-    return dataframe[column].value_counts(dropna=False)
+    return dataframe[column].value_counts(dropna=False).sort_index()
 
 
 def get_sum(dataframe, column):
